b55d70542994151f1577d7786f00280e79bb1b51
root <ssh2611@gmail.com>
2019-05-03 15:56:45 +0000

    update image name

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker run --rm -v $(pwd)/dist:/build/dist -v $(pwd)/alpine.sh:/alpine.sh -it alpine:3.5 sh /alpine.sh
-docker build -t wsc1958/zt-client-final .
-docker container create --device=/dev/net/tun --net=host  --cap-add=NET_ADMIN --cap-add=SYS_ADMIN  --name zt-client-final -v /var/lib/zerotier-one:/var/lib/zerotier-one wsc1958/zt-client-final
+docker build -t wscs1958/zt-client-final .
+docker container create --device=/dev/net/tun --net=host  --cap-add=NET_ADMIN --cap-add=SYS_ADMIN  --name zt-client-final -v /var/lib/zerotier-one:/var/lib/zerotier-one wscs1958/zt-client-final
